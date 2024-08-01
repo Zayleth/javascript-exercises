4bf7ff5c35eccdc3bf82d82878df9f0641eaecb8
Zayleth <zaylethvegas@gmail.com>
2024-07-31 21:54:33 -0400

    Exercise 1 - Return the string 'Hello, World!'

--- 01_helloWorld/helloWorld.js
+++ 01_helloWorld/helloWorld.js
@@ -1,4 +1,5 @@
 // The plain javascript file is where you'll write your code
+// to run the exercises in the terminal `npm test helloWorld.spec.js`
 
 const helloWorld = function() {
   return 'Hello, World!'
