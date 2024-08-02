d87fd79b9d65d896ec131e76913592978a29f9a7
Zayleth <zaylethvegas@gmail.com>
2024-08-01 23:44:28 -0400

    Exercise 4 - using .filter method and parameters (...theArgs)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -8,15 +8,15 @@ module.exports = removeFromArray;
 
 /*
 array = array's name
-array.filter -> method that creates a new array by filtering the elements of the original array (array) based on a condition.
-element = (parameter that accepts the filter method) it represents each element of my original array (array).
+array.filter -> method that creates a new array by filtering the elements of the original array (array) based on a condition
+element = (parameter that accepts the filter method) it represents each element of my original array (array)
 
 CONDITION -
 !theArgs.includes(element)
 
 explains that -
-If the element is not in theArgs, the function returns true and prints the value as is.
-If the element is in theArgs, the function returns false and removes the value from the array.
+If the element is not in theArgs, the function returns true and prints the value as is
+If the element is in theArgs, the function returns false and removes the value from the array
 
 Ex - 
 expect(removeFromArray([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
