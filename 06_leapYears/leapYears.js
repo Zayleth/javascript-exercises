b99ac1b3e0d1ab993e5edae75ff67940458cf405
Zayleth <zaylethvegas@gmail.com>
2024-08-04 00:02:09 -0400

    Exercise 6 - using a function with YEAR as a parameter and CONDITIONS to check if a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,30 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    const divisibleBy4 = year % 4 === 0;
+    const divisibleBy100 = year % 100 === 0;
+    const divisibleBy400 = year % 400 === 0;
+
+    if (divisibleBy4 && !divisibleBy100) {
+        return true;
+
+    } else if (divisibleBy100 && divisibleBy400) {
+        return true;
+
+    } else {
+        return false;
+    }
 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// PARAMETER -> year
+
+/* General conditions to know if a year is a leap year
+
+- Divisible by 4, but not divisible by 100 = LEAP -> TRUE
+- Divisible by 100 and divisible by 400 = LEAP -> TRUE 
+- ELSE = NO LEAP -> FALSE
+
+*/
