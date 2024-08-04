c111829c76829003d198a50ed270c28b2dc2043a
Zayleth <zaylethvegas@gmail.com>
2024-08-04 14:07:52 -0400

    Exercise 7 - using the formulas (Celsius & Fahrenheit) and Math.round to convert to a single decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return (Math.round(((fahrenheit - 32) * 5 / 9) * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return (Math.round((celsius * 9 / 5 + 32) * 10) / 10);
 };
 
 // Do not edit below this line
@@ -9,3 +11,20 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+/*
+
+ANOTHER OPTION:
+The toFixed() method returns a string "", not a number.
+We have to add the parseFloat method to convert the string into a number.
+
+From Fahrenheit to Celsius:
+  const result_celcius = (fahrenheit - 32) * 5 / 9;
+  return parseFloat(result_celcius.toFixed(1));
+
+From Celsius to Fahrenheit:
+  const result_fahrenheit = (celcius * 9 / 5 + 32);
+  return parseFloat(result_fahrenheit.toFixed(1));
+
+*/
