612c59e17404d2cb7ef26ed9d19c8cf38ba871b5
Zayleth <zaylethvegas@gmail.com>
2024-08-10 23:54:56 -0400

    Exercise 10 - new array and formula created to find the new number based on Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    if (number < 0) {
+        return "OOPS";
+
+    } else if (number === 0) {
+        return 0;
+
+    } else {
+        let fib_array = [0, 1];
+
+        for (let i = 2; i <= number; i++) {
+            fib_number = fib_array[i-1] + fib_array[i-2];
+            fib_array.push(fib_number);
+        }
+        return fib_array[number];
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+/*
+Fn = Fn-1 + Fn-2
+WHILE
+n ≥ 2.
+
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025]
+*/
